cd4ca844cfdfbd7d54d39ef246ac3511c3968747
Chengrui Fu <chengrui.fu@duke.edu>
2018-11-08 00:41:52 -0500

    changes

--- crimemap/src/App.js
+++ crimemap/src/App.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import About from './menu/About';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import { Header } from './menu/Header';
+import MapArea from './MapArea';
 import './App.css';
 
 class App extends React.Component {
@@ -19,6 +20,7 @@ class App extends React.Component {
           cities={['Durham', 'Nashville', 'SF']}
           onCitySelected={city => this.setState({ currentCity: city })}
         />
+        <MapArea />
       </Router>
     );
   }
