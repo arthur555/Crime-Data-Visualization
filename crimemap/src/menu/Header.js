c02a8455e0b0eacb6bd43c9d7a9bd3a6f5e6bdf2
Chengrui Fu <chengrui.fu@duke.edu>
2018-11-07 20:49:20 -0500

    changes

--- crimemap/src/menu/Header.js
+++ crimemap/src/menu/Header.js
@@ -1,32 +1,58 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './Header.css';
 import logo from './Crime_Map_Logo.jpg';
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
 
-class NavbarInstance extends React.Component {
-  render() {
-    return (
-      <Navbar>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <a href="#">
-              <img src={logo} className="menu_logo" />
-            </a>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          <NavDropdown title="Menu" />
-        </Nav>
-      </Navbar>
-    );
-  }
-}
+import styled from 'react-emotion';
 
-class Header extends React.Component {
-  render() {
-    return <NavbarInstance />;
-  }
+const Navbar = styled('div')`
+  width: 100%;
+  background-color: black;
+  height: 60px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Logo = styled('img')`
+  max-width: 100%;
+  max-height: 100%;
+`;
+
+const FilterToolbox = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FilterText = styled('div')`
+  color: white;
+  text-align: center;
+`;
+
+const SelectionRow = styled('div')`
+  display: flex;
+`;
+
+const Selection = styled('button')``;
+
+const CitySelector = styled('div')`
+  background-color: pink;
+  width: 100px;
+`;
+
+function Header() {
+  return (
+    <Navbar>
+      <Logo src={logo} />
+      <FilterToolbox>
+        <FilterText>Filter by</FilterText>
+        <SelectionRow>
+          <Selection>AAA</Selection>
+          <Selection>AAA</Selection>
+          <Selection>AAA</Selection>
+          <Selection>AAA</Selection>
+        </SelectionRow>
+      </FilterToolbox>
+      <CitySelector />
+    </Navbar>
+  );
 }
 
 export { Header };
