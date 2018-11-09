b2ca93b1af87c529449e81bbb770553d547c6778
Pratik Mulpury <pratik@Pratiks-MacBook-Pro.local>
2018-11-08 21:38:24 -0500

    pratik map commit

--- crimemap/src/App.js
+++ crimemap/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import About from './menu/About';
+//import About from './menu/About';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Header } from './menu/Header';
-import MapArea from './MapArea';
+import {Header}  from './menu/Header';
+import {MapArea} from './MapArea';
 import './App.css';
 
 class App extends React.Component {
@@ -15,11 +15,16 @@ class App extends React.Component {
     const { currentCity } = this.state;
     return (
       <Router>
-        <Header
-          currentCity={currentCity}
-          cities={['Durham', 'Nashville', 'SF']}
-          onCitySelected={city => this.setState({ currentCity: city })}
-        />
+        <div>
+            <Header
+              currentCity={currentCity}
+              cities={['Durham', 'Nashville', 'SF']}
+              onCitySelected={city => this.setState({ currentCity: city })}
+            />
+            <div id = "map">
+              <MapArea></MapArea>
+            </div>
+          </div>
       </Router>
     );
   }
--- crimemap/src/MapArea.js
+++ crimemap/src/MapArea.js
@@ -1,33 +1,47 @@
-import React from 'react';
-import styled from 'react-emotion';
-// import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
-// import HeatmapLayer from '../src/HeatmapLayer';
-// import { addressPoints } from './realworld.10000.js';
+import React, { Component } from 'react'
+const center = [51.505, -0.09]
 
-const MapBox = styled('div')``;
+/*
+      <Map center={center} zoom={13} style ={{ flex:1, height:800}}>
+        <TileLayer
+          attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </Map>
+*/
 
-// class MapArea extends React.Component {
-//   render() {
-//     return (
-//       <div>HI</div>
-//       // <MapBox>
-//       //   <Map center={[0, 0]} zoom={13}>
-//       //     <HeatmapLayer
-//       //       fitBoundsOnLoad
-//       //       fitBoundsOnUpdate
-//       //       points={addressPoints}
-//       //       longitudeExtractor={m => m[1]}
-//       //       latitudeExtractor={m => m[0]}
-//       //       intensityExtractor={m => parseFloat(m[2])}
-//       //     />
-//       //     <TileLayer
-//       //       url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-//       //       attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
-//       //     />
-//       //   </Map>
-//       // </MapBox>
-//     );
-//   }
-// }
+/*
 
-// export default { MapArea };
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v8',
+    center: [-78.89, 35.99],
+    zoom: 12,
+})
+map.fitBounds([[
+    -78.89,
+    -35.99
+]]);
+
+*/
+
+
+
+export default class MapArea extends React.Component {
+  render() {
+    var map = L.map('mapid').setView([center]);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1Ijoic2hpbWl6dSIsImEiOiI0cl85c2pNIn0.RefZMaOzNn-IistVe-Zcnw'
+    }).addTo(map);
+
+    return (
+        <div id="mapid"></div>
+    );
+  }
+}
+export { MapArea };
+
